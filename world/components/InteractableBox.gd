cdd7ecc4c514c64d900c2a125dbed6ca94adbd97
JDM-Blimy <44740917+JDM-Blimy@users.noreply.github.com>
2020-12-06 16:05:59 +0800

    doors and walls

--- world/components/InteractableBox.gd
+++ world/components/InteractableBox.gd
@@ -9,7 +9,7 @@ var owner_activation_callback_fn: String = "activate"
 func _ready():
 	assert(owning_entity_path != null, "Tried to initialise un-owned hitbox :{")
 	owning_entity = get_node(owning_entity_path)
-	assert(owning_entity != null, "ur stupid")
+	assert(owning_entity != null, "ur really stupid")
 
 func interact():
 	owning_entity.call(owner_activation_callback_fn)
