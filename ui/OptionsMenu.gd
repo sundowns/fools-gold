0fd0602234630d6690b50290c7f5d4100d1eda83
unknown <max.chappos@gmail.com>
2020-12-08 17:00:53 +0800

    Added window mode option to options menu

--- ui/OptionsMenu.gd
+++ ui/OptionsMenu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+onready var fullscreen_menubutton = $FullScreen/MenuButton
+
 signal options_closed
 signal sensitivity_changed(new_sens_multiplier)
 
@@ -8,6 +10,10 @@ func _ready():
 	connect("sensitivity_changed", Global, "_on_sensitivity_changed")
 	$Sensitivity/HSlider.value = Global.sens_multiplier
 	pause_mode = Node.PAUSE_MODE_PROCESS
+	
+	fullscreen_menubutton.get_popup().add_item("Windowed")
+	fullscreen_menubutton.get_popup().add_item("Full Screen")
+	fullscreen_menubutton.get_popup().connect("id_pressed", self, "_on_item_pressed")
 
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_cancel"):
@@ -22,3 +28,12 @@ func _on_BackButton_pressed():
 
 func _on_HSlider_value_changed(value):
 	emit_signal("sensitivity_changed", value)
+	
+func _on_item_pressed(item_id):
+	var item_name = fullscreen_menubutton.get_popup().get_item_text(item_id)
+	fullscreen_menubutton.set_text(item_name)
+	match item_name:
+		"Windowed":
+			print("Put windowed code here")
+		"Full Screen":
+			print("Put fullscreen code here")
