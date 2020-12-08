e484b0787a67786c997c3da98669c7dff6b7433b
unknown <max.chappos@gmail.com>
2020-12-08 19:55:55 +0800

    Fixed Audio3D not scaling to global volume on ready

--- audio/Audio3D.gd
+++ audio/Audio3D.gd
@@ -5,6 +5,7 @@ onready var base_unit_db = unit_db
 func _ready():
 # warning-ignore:return_value_discarded
 	Global.connect("master_volume_changed", self, "_on_master_volume_changed")
+	unit_db = linear2db(db2linear(base_unit_db) * Global.master_volume)
 
 func _on_master_volume_changed(val):
 	unit_db = linear2db(db2linear(base_unit_db) * val)
