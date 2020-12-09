f7f493ae9f73364b48a4f307521f520a66776806
Tom <tom@smallridge.com.au>
2020-12-09 19:55:55 +0800

    weapon pickups wip

--- shared/weapon_unlocks.gd
+++ shared/weapon_unlocks.gd
@@ -11,6 +11,7 @@ signal unlocked_weapon(weapon_key)
 func unlock(key: String):
 	if unlocks.has(key):
 		unlocks[key] = true
+		print("unlocked: %s" %key)
 		emit_signal("unlocked_weapon", key)
 	else:
 		print("tried to unlock imaginary weapon %s" % key)
