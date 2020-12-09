66d6c82d1f26bd8cb2b91009dd64e78b9109c624
Tom <tom@smallridge.com.au>
2020-12-09 19:28:03 +0800

    wip weapon unlocks

---
+++ shared/weapon_unlocks.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var unlocks: Dictionary = {
+	"revolver": true,
+	"shotgun": false,
+	"dual_revolvers": true
+}
+
+signal unlocked_weapon(weapon_key)
+
+func unlock(key: String):
+	if unlocks.has(key):
+		unlocks[key] = true
+		emit_signal("unlocked_weapon", key)
+	else:
+		print("tried to unlock imaginary weapon %s" % key)
