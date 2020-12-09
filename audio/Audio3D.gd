4de69faa907fcde70b276ff34f27097d9bb7a2e1
unknown <max.chappos@gmail.com>
2020-12-10 06:56:15 +0800

    Added boolean to Audio3D and ScaledAudio for volume slider designations

--- audio/Audio3D.gd
+++ audio/Audio3D.gd
@@ -1,13 +1,20 @@
 extends AudioStreamPlayer3D
 
+export(bool) var is_sfx = true
 onready var base_unit_db = unit_db
 
 func _ready():
 # warning-ignore:return_value_discarded
 	Global.connect("volume_changed", self, "_on_volume_changed")
-	unit_db = linear2db(db2linear(base_unit_db) * (Global.master_volume * Global.sfx_volume))
+	if is_sfx:
+		unit_db = linear2db(db2linear(base_unit_db) * (Global.master_volume * Global.sfx_volume))
+	else:
+		unit_db = linear2db(db2linear(base_unit_db) * (Global.master_volume * Global.music_volume))
 
-func _on_volume_changed(master_vol, sfx_vol, _music_vol):
-	unit_db = linear2db(db2linear(base_unit_db) * (master_vol * sfx_vol)) 
+func _on_volume_changed(master_vol, sfx_vol, music_vol):
+	if is_sfx:
+		unit_db = linear2db(db2linear(base_unit_db) * (master_vol * sfx_vol)) 
+	else:
+		unit_db = linear2db(db2linear(base_unit_db) * (master_vol * music_vol)) 
 	
 	
--- audio/ScaledAudio.gd
+++ audio/ScaledAudio.gd
@@ -1,11 +1,18 @@
 extends AudioStreamPlayer
 
+export(bool) var is_sfx = true
 onready var base_volume_db = volume_db
 
 func _ready():
 # warning-ignore:return_value_discarded
 	Global.connect("volume_changed", self, "_on_volume_changed")
-	volume_db = linear2db(db2linear(base_volume_db) * (Global.master_volume * Global.music_volume))
+	if is_sfx:
+		volume_db = linear2db(db2linear(base_volume_db) * (Global.master_volume * Global.sfx_volume))
+	else:
+		volume_db = linear2db(db2linear(base_volume_db) * (Global.master_volume * Global.music_volume))
 
-func _on_volume_changed(master_vol, _sfx_vol, music_vol):
-	volume_db = linear2db(db2linear(base_volume_db) * (master_vol * music_vol))
+func _on_volume_changed(master_vol, sfx_vol, music_vol):
+	if is_sfx:
+		volume_db = linear2db(db2linear(base_volume_db) * (master_vol * sfx_vol))
+	else:
+		volume_db = linear2db(db2linear(base_volume_db) * (master_vol * music_vol))
